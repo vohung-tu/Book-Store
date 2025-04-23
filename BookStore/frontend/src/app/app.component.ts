2c4a418b6199869fee280b1f4faabfe8b1932ae6
vohung-tu <56868551+vohung-tu@users.noreply.github.com>
2025-04-23 20:23:39 +0700

    update code

--- BookStore/frontend/src/app/app.component.ts
+++ BookStore/frontend/src/app/app.component.ts
@@ -33,7 +33,8 @@ export class AppComponent {
   isAdminPage(): boolean {
     const url = this.router.url;
     // Mảng các route bạn muốn kiểm tra
-    const prefixes = ['/admin', '/signin', '/signup'];
-    return prefixes.some(prefix => url.startsWith(prefix));
+    const prefixes = ['/signin', '/signup'];
+    const pref = ['/admin'];
+    return [prefixes, pref].some(prefix => url.startsWith([prefix]));
   }
 }
