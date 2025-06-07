20dd09a074c2247a2b47586ff19c0b1b64947b8f
vohung-tu <vohungtu2000@gmail.com>
2025-06-07 17:40:35 +0700

    fix vercel

---
+++ BookStore/frontend/routes-ids.ts
@@ -0,0 +1,16 @@
+export const bookId: string[] = [
+    '67d476b5bc4adca790816958',
+    '67d476cebc4adca790816959',
+    '67d47739bc4adca79081695a',
+    '67d47799bc4adca79081695b',
+    '67d477d6bc4adca79081695c',
+
+];
+
+export const authorId: string[] = [
+    '682a2f460e153123a0e583da',
+    '682a2f080e153123a0e583d7',
+    '682a2e9e0e153123a0e583b4',
+    '682a2a8b0e153123a0e58244',
+    '682a2a150e153123a0e58221',
+]
--- BookStore/frontend/src/app/app.routes.server.ts
+++ BookStore/frontend/src/app/app.routes.server.ts
@@ -1,6 +1,31 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { authorId, bookId } from '../../routes-ids';
+const categoryNames = ['sach-trong-nuoc', 'manga', 'vpp-dung-cu-hoc-sinh', 'do-choi', 'lam-dep', 'sach-tham-khao', 'sach-ngoai-van'];
 
 export const serverRoutes: ServerRoute[] = [
+  { 
+    path: 'category/:categoryName', 
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return categoryNames.map(categoryName => ({ categoryName }));
+    },
+  },
+  { 
+    path: 'details/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = bookId;
+      return ids.map(id => ({ id }));
+    },
+  },
+  { 
+    path: 'author/:id', 
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = authorId;
+      return ids.map(id => ({ id }));
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
