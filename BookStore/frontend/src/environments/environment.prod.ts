5f6526bf43cd528d7e1295ad4bb225bd4a986e49
vohung-tu <56868551+vohung-tu@users.noreply.github.com>
2025-06-09 21:32:50 +0700

    test

--- BookStore/frontend/src/environments/environment.prod.ts
+++ BookStore/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api.vercel.app',  // Đổi thành domain backend thật
-  frontendUrl: 'https://book-store.vercel.app' // Domain frontend trên Vercel
+  apiUrl: 'https://book-store-3-svnz.onrender.com',  // Đổi thành domain backend thật
+
 };
